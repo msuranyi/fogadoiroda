c686ec18144009701f63713fcd33c77044d0081a
Marcell Suranyi <marcell.suranyi@gmail.com>
2015-11-04 23:05:35 +0100

    SecurityController-ből nem oda való dolgok áthelyezése (DefaultController-be)

---
+++ src/main/java/hu/gdf/oop/fogadoiroda/web/controller/DefaultController.java
@@ -0,0 +1,71 @@
+package hu.gdf.oop.fogadoiroda.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.beans.PropertyEditorSupport;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@ControllerAdvice
+public class DefaultController {
+
+    private final static Logger LOG = LoggerFactory.getLogger(DefaultController.class);
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+        binder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                try {
+                    setValue(new SimpleDateFormat("yyyy.MM.dd HH:mm").parse(text));
+                } catch(ParseException e) {
+                    setValue(null);
+                }
+            }
+
+            @Override
+            public String getAsText() {
+                String result = null;
+                if (getValue() != null) {
+                    result = new SimpleDateFormat("yyyy.MM.dd HH:mm").format((Date) getValue());
+                }
+                return result;
+            }
+        });
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ModelAndView handleError(HttpServletRequest req, Throwable exception) {
+
+        LOG.error("Alkalmazás hiba lépett fel!", exception);
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.addObject("exception", exception);
+        modelAndView.addObject("url", req.getRequestURL());
+        modelAndView.addObject("statusCode", 500);
+        modelAndView.setViewName("error/short");
+
+        return modelAndView;
+    }
+
+    @RequestMapping({"/welcome", "/"})
+    public String welcome(Model model) {
+        model.addAttribute("example", new Example());
+        return "welcome";
+    }
+}
--- src/main/java/hu/gdf/oop/fogadoiroda/web/controller/SecurityController.java
+++ src/main/java/hu/gdf/oop/fogadoiroda/web/controller/SecurityController.java
@@ -1,45 +1,13 @@
 package hu.gdf.oop.fogadoiroda.web.controller;
 
-import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import java.beans.PropertyEditorSupport;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@ControllerAdvice
 public class SecurityController {
-
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
-        binder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
-            @Override
-            public void setAsText(String text) throws IllegalArgumentException {
-                try {
-                    setValue(new SimpleDateFormat("yyyy.MM.dd HH:mm").parse(text));
-                } catch(ParseException e) {
-                    setValue(null);
-                }
-            }
-
-            @Override
-            public String getAsText() {
-                String result = null;
-                if (getValue() != null) {
-                    result = new SimpleDateFormat("yyyy.MM.dd HH:mm").format((Date) getValue());
-                }
-                return result;
-            }
-        });
-    }
 
     @RequestMapping("/login")
     public String login(Model model, @RequestParam(value = "error", required = false) String error) {
@@ -54,12 +22,6 @@ public class SecurityController {
         return "auth/access-denied";
     }
 
-    @RequestMapping({"/welcome", "/"})
-    public String welcome(Model model) {
-        model.addAttribute("example", new Example());
-        return "welcome";
-    }
-
     @RequestMapping("/admin/welcome")
     public String secured(Model model) {
         model.addAttribute("example", new Example());
@@ -71,18 +33,4 @@ public class SecurityController {
         model.addAttribute(example);
         return "example";
     }
-
-    @ExceptionHandler(Throwable.class)
-    public ModelAndView handleError(HttpServletRequest req, Throwable exception) {
-
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.addObject("exception", exception);
-        modelAndView.addObject("url", req.getRequestURL());
-        modelAndView.addObject("statusCode", 500);
-        modelAndView.setViewName("error/short");
-
-        return modelAndView;
-    }
-
 }
