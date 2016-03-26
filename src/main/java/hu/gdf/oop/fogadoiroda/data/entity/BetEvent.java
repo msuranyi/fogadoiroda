78bca3feced61165465295e4510605baf8fa2718
Diószegi Krisztián <Diszegi Krisztin>
2016-03-26 21:12:10 +0100

    BetEvent,Outcome entity,repository

---
+++ src/main/java/hu/gdf/oop/fogadoiroda/data/entity/BetEvent.java
@@ -0,0 +1,51 @@
+package hu.gdf.oop.fogadoiroda.data.entity;
+
+import java.time.LocalDateTime;
+
+public class BetEvent {
+    private Integer id;
+    private Integer userId;
+    private String title;
+    private LocalDateTime created;
+    private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/hu/gdf/oop/fogadoiroda/data/entity/Outcome.java
@@ -0,0 +1,49 @@
+package hu.gdf.oop.fogadoiroda.data.entity;
+
+public class Outcome {
+    private Integer id;
+    private Integer betEventId;
+    private String title;
+    private boolean won;
+    private Integer sumBetAmount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getBetEventId() {
+        return betEventId;
+    }
+
+    public void setBetEventId(Integer betEventId) {
+        this.betEventId = betEventId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isWon() {
+        return won;
+    }
+
+    public void setWon(boolean won) {
+        this.won = won;
+    }
+
+    public Integer getSumBetAmount() {
+        return sumBetAmount;
+    }
+
+    public void setSumBetAmount(Integer sumBetAmount) {
+        this.sumBetAmount = sumBetAmount;
+    }
+}
