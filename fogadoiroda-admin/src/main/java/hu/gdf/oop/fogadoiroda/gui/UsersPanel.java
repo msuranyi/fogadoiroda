d71aa112e7ce41025758444aa06c63aa10f303a0
Diószegi Krisztián <Diszegi Krisztin>
2016-04-05 18:19:35 +0200

    User Panel

--- fogadoiroda-admin/src/main/java/hu/gdf/oop/fogadoiroda/gui/UsersPanel.java
+++ fogadoiroda-admin/src/main/java/hu/gdf/oop/fogadoiroda/gui/UsersPanel.java
@@ -5,16 +5,103 @@
  */
 package hu.gdf.oop.fogadoiroda.gui;
 
+import hu.gdf.oop.fogadoiroda.data.entity.User;
+import hu.gdf.oop.fogadoiroda.data.repository.UserRepository;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author marci
  */
 public class UsersPanel extends javax.swing.JPanel {
 
+    String[] columnNames = {"ID",
+            "Felhasználónév",
+            "Jelszó",
+            "Email",
+            "Jogosultságok",
+            "Státusz",
+            "Egyenleg",
+            "Létrehozva"
+    };
+
+    String[] editableColumns = {
+            "ID",
+            "Felhasználónév",
+            "Jelszó",
+            "Email",
+            "Jogosultságok",
+            "Státusz",
+            "Egyenleg"
+    };
+    
+    UserRepository userRepository = new UserRepository();
+    DataTable userTable;
+    
+    private void setTableData(){
+        List<User> userList = userRepository.findAll();
+        ArrayList<Object[]> data = new ArrayList<Object[]>();
+        for(int i = 0; i<userList.size();i++){
+            data.add(userList.get(i).toArray());
+        }
+        userTable = new DataTable(columnNames,data);
+        userTable.setEditableColumns(editableColumns);
+    }
+    private void addRow(){
+        userTable.addRow(User.getEmptyModel());
+        table.revalidate();
+        table.repaint();
+    }
+
+    private void deleteRow(){
+        userTable.deleteRow(table.getSelectedRow());
+        table.revalidate();
+        table.repaint();
+    }
+
+    private void saveData(){
+        ArrayList<Object[]> data = userTable.data;
+        for(int i=0; i<userTable.rowStatus.size();i++){
+            DataTable.RowStatus status = userTable.rowStatus.get(i);
+            boolean change = (status == DataTable.RowStatus.CHANGED || status == DataTable.RowStatus.DISABLED);
+            if(change){
+                int userId = (int)(data.get(i)[0]);
+                User user = userRepository.findOne(userId);
+                if(status == DataTable.RowStatus.DISABLED){
+                    userRepository.delete(user);
+                }else{
+                    if(user == null){
+                        user = new User();
+                        user.setId((int)data.get(i)[0]);
+                        user.setUsername((String)data.get(i)[1]);
+                        user.setPassword((String) data.get(i)[2]);
+                        user.setEmail((String) data.get(i)[3]);
+                        user.setAuthority((String) data.get(i)[4]);
+                        user.setActive((boolean) data.get(i)[5]);
+                        user.setBalance((int)data.get(i)[6]);
+                        user.setCreated(LocalDateTime.now());
+                        userRepository.create(user);
+                    }else{
+                        user.setUsername((String)data.get(i)[1]);
+                        user.setPassword((String) data.get(i)[2]);
+                        user.setEmail((String) data.get(i)[3]);
+                        user.setAuthority((String) data.get(i)[4]);
+                        user.setActive((boolean) data.get(i)[5]);
+                        user.setBalance((int)data.get(i)[6]);
+                        userRepository.update(user);
+                    }
+                }
+            }
+        }
+    }
+    
     /**
      * Creates new form UsersPanel
      */
     public UsersPanel() {
+        setTableData();
         initComponents();
     }
 
@@ -28,7 +115,7 @@ public class UsersPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        table = new javax.swing.JTable();
         controlPanel = new javax.swing.JPanel();
         btnSave = new javax.swing.JButton();
         btnCreate = new javax.swing.JButton();
@@ -36,26 +123,31 @@ public class UsersPanel extends javax.swing.JPanel {
 
         setLayout(new java.awt.BorderLayout());
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        table.setModel(userTable);
+        jScrollPane1.setViewportView(table);
 
         add(jScrollPane1, java.awt.BorderLayout.CENTER);
 
         btnSave.setText("Mentés");
+        btnSave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSaveActionPerformed(evt);
+            }
+        });
 
         btnCreate.setText("Létrehozás");
+        btnCreate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCreateActionPerformed(evt);
+            }
+        });
 
         btnDelete.setText("Törlés");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout controlPanelLayout = new javax.swing.GroupLayout(controlPanel);
         controlPanel.setLayout(controlPanelLayout);
@@ -84,6 +176,18 @@ public class UsersPanel extends javax.swing.JPanel {
         add(controlPanel, java.awt.BorderLayout.SOUTH);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
+       saveData();
+    }//GEN-LAST:event_btnSaveActionPerformed
+
+    private void btnCreateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCreateActionPerformed
+        addRow();
+    }//GEN-LAST:event_btnCreateActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        deleteRow();
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCreate;
@@ -91,6 +195,6 @@ public class UsersPanel extends javax.swing.JPanel {
     private javax.swing.JButton btnSave;
     private javax.swing.JPanel controlPanel;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable table;
     // End of variables declaration//GEN-END:variables
 }
