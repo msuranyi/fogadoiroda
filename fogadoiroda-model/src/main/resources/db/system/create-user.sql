1ee27626bda7b3d3b49f82ca8a54bb63b3f8e38b
Marcell Suranyi <marcell.suranyi@gmail.com>
2016-04-05 13:51:54 +0200

    liquibase beépítése DB építéshez

---
+++ fogadoiroda-model/src/main/resources/db/system/create-user.sql
@@ -0,0 +1,7 @@
+drop user fogadoiroda cascade;
+
+CREATE USER fogadoiroda IDENTIFIED BY fogadoiroda PROFILE DEFAULT ACCOUNT UNLOCK;
+GRANT CONNECT TO fogadoiroda;
+GRANT RESOURCE TO fogadoiroda;
+GRANT UNLIMITED TABLESPACE TO fogadoiroda;
+GRANT ADMINISTER DATABASE TRIGGER TO fogadoiroda;
