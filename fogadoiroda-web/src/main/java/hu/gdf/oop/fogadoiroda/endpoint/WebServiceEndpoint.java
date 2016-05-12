bba806292a3c3f038f4702a0d34cb80dc3504ede
Marcell Suranyi <marcell.suranyi@gmail.com>
2016-05-12 23:30:07 +0200

    toXmlDate bugfix

--- fogadoiroda-web/src/main/java/hu/gdf/oop/fogadoiroda/endpoint/WebServiceEndpoint.java
+++ fogadoiroda-web/src/main/java/hu/gdf/oop/fogadoiroda/endpoint/WebServiceEndpoint.java
@@ -1,97 +1,99 @@
 package hu.gdf.oop.fogadoiroda.endpoint;
 
-import hu.gdf.oop.fogadoiroda.model.*;
 import hu.gdf.oop.fogadoiroda.service.EventService;
 import hu.gdf.oop.fogadoiroda.service.OutcomeService;
-import hu.gdf.oop.fogadoiroda.xml.*;
+import hu.gdf.oop.fogadoiroda.xml.Echo;
+import hu.gdf.oop.fogadoiroda.xml.EchoResponse;
 import hu.gdf.oop.fogadoiroda.xml.Event;
+import hu.gdf.oop.fogadoiroda.xml.Events;
+import hu.gdf.oop.fogadoiroda.xml.ObjectFactory;
+import hu.gdf.oop.fogadoiroda.xml.OpenEvents;
+import hu.gdf.oop.fogadoiroda.xml.OpenEventsResponse;
 import hu.gdf.oop.fogadoiroda.xml.Outcome;
-import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
-import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-
+import hu.gdf.oop.fogadoiroda.xml.Outcomes;
+import java.util.Collection;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import javax.annotation.Resource;
 import javax.xml.bind.JAXBElement;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Collection;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class WebServiceEndpoint {
 
-    private ObjectFactory factory = new ObjectFactory();
+	private ObjectFactory factory = new ObjectFactory();
 
-    @Resource
-    private EventService eventService;
+	@Resource
+	private EventService eventService;
 
-    @Resource
-    private OutcomeService outcomeService;
+	@Resource
+	private OutcomeService outcomeService;
 
-    @PayloadRoot(namespace = "http://xml.fogadoiroda.oop.gdf.hu/", localPart = "echo")
-    @ResponsePayload
-    public JAXBElement<EchoResponse> echo(@RequestPayload JAXBElement<Echo> echo) {
+	@PayloadRoot(namespace = "http://xml.fogadoiroda.oop.gdf.hu/", localPart = "echo")
+	@ResponsePayload
+	public JAXBElement<EchoResponse> echo(@RequestPayload JAXBElement<Echo> echo) {
 
-        EchoResponse response = factory.createEchoResponse();
+		EchoResponse response = factory.createEchoResponse();
 
-        response.setGeneratedTime(toXmlDate(null));
-        response.setReturn("Echo - " + echo.getValue().getString());
+		response.setGeneratedTime(toXmlDate(null));
+		response.setReturn("Echo - " + echo.getValue().getString());
 
-        return factory.createEchoResponse(response);
-    }
+		return factory.createEchoResponse(response);
+	}
 
-    @PayloadRoot(namespace = "http://xml.fogadoiroda.oop.gdf.hu/", localPart = "openEvents")
-    @ResponsePayload
-    public JAXBElement<OpenEventsResponse> openEvents(@RequestPayload JAXBElement<OpenEvents> openEvents) {
+	@PayloadRoot(namespace = "http://xml.fogadoiroda.oop.gdf.hu/", localPart = "openEvents")
+	@ResponsePayload
+	public JAXBElement<OpenEventsResponse> openEvents(@RequestPayload JAXBElement<OpenEvents> openEvents) {
 
-        OpenEventsResponse response = factory.createOpenEventsResponse();
+		OpenEventsResponse response = factory.createOpenEventsResponse();
 
-        response.setGeneratedTime(toXmlDate(null));
-        Events events = factory.createEvents();
-        eventService.findAllOpen().forEach(e -> {
-            Event event = factory.createEvent();
-            event.setId(e.getId());
-            event.setTitle(e.getTitle());
-            event.setDescription(e.getDescription());
-            event.setStart(toXmlDate(e.getStart()));
-            event.setEnd(toXmlDate(e.getEnd()));
-            Outcomes outcomes = factory.createOutcomes();
+		response.setGeneratedTime(toXmlDate(null));
+		Events events = factory.createEvents();
+		eventService.findAllOpen().forEach(e -> {
+			Event event = factory.createEvent();
+			event.setId(e.getId());
+			event.setTitle(e.getTitle());
+			event.setDescription(e.getDescription());
+			event.setStart(toXmlDate(e.getStart()));
+			event.setEnd(toXmlDate(e.getEnd()));
+			Outcomes outcomes = factory.createOutcomes();
 
-            Collection<hu.gdf.oop.fogadoiroda.model.Outcome> eventOutcomes = outcomeService.findByBetEventId(event.getId());
-            eventOutcomes.forEach((v) -> {
-                Outcome outcome = factory.createOutcome();
-                outcome.setId(v.getId());
-                outcome.setTitle(v.getTitle());
-                outcomes.getOutcome().add(outcome);
-            });
-            event.setOutcomes(outcomes);
-            events.getEvent().add(event);
-        });
-        response.setEvents(events);
+			Collection<hu.gdf.oop.fogadoiroda.model.Outcome> eventOutcomes = outcomeService.findByBetEventId(
+				event.getId());
+			eventOutcomes.forEach((v) -> {
+				Outcome outcome = factory.createOutcome();
+				outcome.setId(v.getId());
+				outcome.setTitle(v.getTitle());
+				outcomes.getOutcome().add(outcome);
+			});
+			event.setOutcomes(outcomes);
+			events.getEvent().add(event);
+		});
+		response.setEvents(events);
 
-        return factory.createOpenEventsResponse(response);
-    }
+		return factory.createOpenEventsResponse(response);
+	}
 
-    private XMLGregorianCalendar toXmlDate(Date date) {
+	private XMLGregorianCalendar toXmlDate(Date date) {
 
-        ZonedDateTime zdt;
-        if (date != null) {
-            zdt = date.toInstant().atZone(ZoneId.systemDefault());
-        } else {
-            zdt = LocalDateTime.now().atZone(ZoneId.systemDefault());
-        }
+		GregorianCalendar gc = new GregorianCalendar();
+		if (date != null) {
+			gc.setTime(date);
+//		} else {
+//			zdt = LocalDateTime.now().atZone(ZoneId.systemDefault());
+		}
 
-        try {
-            return DatatypeFactory.newInstance().newXMLGregorianCalendar(GregorianCalendar.from(zdt));
-        } catch (DatatypeConfigurationException e) {
-            throw new RuntimeException("Nem várt hiba történt.");
-        }
-    }
+		try {
+			return DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
+		} catch (DatatypeConfigurationException e) {
+			throw new RuntimeException("Nem várt hiba történt.");
+		}
+	}
 
 }
