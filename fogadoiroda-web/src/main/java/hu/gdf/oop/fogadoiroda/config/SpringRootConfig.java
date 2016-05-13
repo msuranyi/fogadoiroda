ddeb60d3a2b5b3aeae1dc4300b43527858f5e47e
Marcell Suranyi <marcell.suranyi@gmail.com>
2016-05-13 15:45:23 +0200

    javadoc fix

--- fogadoiroda-web/src/main/java/hu/gdf/oop/fogadoiroda/config/SpringRootConfig.java
+++ fogadoiroda-web/src/main/java/hu/gdf/oop/fogadoiroda/config/SpringRootConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
  * String ApplicationContext felépítése az alkalmazás indulásakor.
  * <br><br>
  * Explicit komponensek nincsekek itt deklarálva, viszont a hu.gdf.oop.fogadoiroda.service és
- * hu.gdf.oop.fogadoiroda.repository.memory csomagokra be van kapcsolva a komponensek szkennelése,
+ * hu.gdf.oop.fogadoiroda.repository.jdbc csomagokra be van kapcsolva a komponensek szkennelése,
  * tehát minden ott található, <code>Component</code> vagy <code>Service</code> annotációval ellátott
  * osztályból létrejön egy függőségeivel teljesen felszerelt komponens.
  */
